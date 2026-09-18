8a47d0ffe077f6190769c47f21ac904de1f67b7f
akashs-bit <sbakash434@gmail.com>
2026-09-18 15:25:56 +0530

    Fix jobs API CORS

--- backend/api/jobs/get.php
+++ backend/api/jobs/get.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: https://job-tracker-drab-mu.vercel.app");
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
